80b7ed2e04f645155937c6b1ccc8607436c86bc3
Oluwasola <sabiduria449@gmail.com>
2025-08-16 21:29:28 +0100

    Form Validation

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
         // Retrieve and trim inputs
         const username = document.getElementById("username").value.trim();
         const email = document.getElementById("email").value.trim();
-        const password1 = document.getElementById("password").value.trim();
+        const password = document.getElementById("password").value.trim();
 
         let isValid = true;
         let messages = [];
